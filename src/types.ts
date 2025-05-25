1a18324a2f76179cac32a1ec7971d20606d2b4a5
moonlitgrace <moonlitgrace.gaia@gmail.com>
2025-05-25 22:44:19 +0530

    chore: loadConfig func

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import { Connection } from "mongoose";
+import { Connection, ConnectOptions } from "mongoose";
 import { Document } from "mongoose";
 
 export interface MigrationModel extends Document {
@@ -15,4 +15,10 @@ export interface MigrationFile {
   name: string;
   up(connection: Connection): Promise<void>;
   down(connection: Connection): Promise<void>;
+}
+
+export interface MigrationConfig {
+  connectionUri: string;
+  migrationsPath?: string;
+  options: ConnectOptions;
 }
